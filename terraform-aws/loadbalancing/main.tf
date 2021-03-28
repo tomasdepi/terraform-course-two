6d45d6acf05fca571f5f19c4601517f7cc2d63a4
Tomas De Pietro <tomas.depietro@wearepsh.com>
2021-03-28 20:51:59 -0300

    add target_group and lb_listener

--- terraform-aws/loadbalancing/main.tf
+++ terraform-aws/loadbalancing/main.tf
@@ -6,3 +6,27 @@ resource "aws_lb" "lb" {
   idle_timeout    = 400
 }
 
+resource "aws_lb_target_group" "tg" {
+  name     = "lb-tg-${substr(uuid(), 0, 3)}"
+  port     = var.tg_port
+  protocol = var.tg_protocol
+  vpc_id   = var.vpc_id
+
+  health_check {
+    healthy_threshold   = var.lb_healthy_treshold
+    unhealthy_threshold = var.lb_unhealthy_treshold
+    timeout             = var.lb_timeout
+    interval            = var.lb_interval
+  }
+}
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
