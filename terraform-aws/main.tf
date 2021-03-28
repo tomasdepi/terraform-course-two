2bc819460763a37c8a0189eda2c38b8db1a6964a
Tomas De Pietro <tomas.depietro@wearepsh.com>
2021-03-27 23:10:13 -0300

    use local values

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,10 +1,14 @@
 
+locals {
+  vpc_cidr = "10.0.0.0/16"
+}
+
 module "vpc" {
   source = "./networking"
 
-  vpc_cidr             = "10.0.0.0/16"
+  vpc_cidr             = local.vpc_cidr
   public_subnet_count  = 2
   private_subnet_count = 3
-  public_cidrs         = [for i in range(2, 255, 2) : cidrsubnet("10.0.0.0/16", 8, i)]
-  private_cidrs        = [for i in range(1, 255, 2) : cidrsubnet("10.0.0.0/16", 8, i)]
+  public_cidrs         = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
+  private_cidrs        = [for i in range(1, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
 }
